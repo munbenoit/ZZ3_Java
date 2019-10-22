fee7901677c79a0eadfd8b4aa51e58fc23749e90
bemunoz2 <bemunoz2@turing.local.isima.fr>
2019-10-22 09:57:33 +0200

    Add GSON

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,35 @@
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import com.google.gson.Gson;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Stub de la méthode généré automatiquement
+		HttpURLConnection connectioUrl = null;
+		try {
+			URL url = new URL("https://api.openweathermap.org/data/2.5/weather?q=clermont-ferrand&appid=c62ff9852f25d0252f6681f2d0205609");
+			connectioUrl = (HttpURLConnection) url.openConnection();
+			InputStream  in = new  BufferedInputStream(connectioUrl.getInputStream ());
+			BufferedReader test = new BufferedReader(new InputStreamReader(in));
+			Gson gson = new Gson();
+			gson.fromJson(test, Meteo.class);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}finally {
+			if (connectioUrl != null){
+				connectioUrl.disconnect ();
+			}
+		}
+		
+		
 
 	}
 
---
+++ src/Meteo.java
@@ -0,0 +1,12 @@
+import java.util.Date;
+
+public class Meteo {
+	private Date date;
+	private Statistics main;
+	
+	private class Statistics{
+		private double temp;
+	}
+
+	
+}
